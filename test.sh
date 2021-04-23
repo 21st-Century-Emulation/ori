a16de8cf89b9cd92f26a2e3cddf46dff2784fc64
David Tyler <david.tyler@metaswitch.com>
2021-04-23 14:27:53 +0100

    Renamed operands to be consistent between microservices

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ docker run --rm --name ori -d -p 8080:8080 ori
 RESULT=`curl -s --header "Content-Type: application/json" \
   --request POST \
   --data '{"opcode":246,"state":{"a":181,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":1,"stackPointer":2,"cycles":0}}' \
-  http://localhost:8080/api/v1/execute?operand=15`
+  http://localhost:8080/api/v1/execute?operand1=15`
 EXPECTED='{"opcode":246,"state":{"a":191,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":true,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":1,"stackPointer":2,"cycles":7}}'
 
 docker kill ori
